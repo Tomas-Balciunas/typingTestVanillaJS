91cc2ba728e30b10c5a6ae02ed3fb0b94bbbae5f
Tomas-Balciunas <tomas.balciunas@stud.kitm.lt>
2023-12-19 19:06:10 +0200

    implemented timer

--- script.js
+++ script.js
@@ -6,6 +6,70 @@ let placeholder = document.querySelector('#placeholder');
 
 const allowed = ['Backspace', 'Escape', 'Enter']
 
+async function main() {
+    wrapText();
+    let timerStarted = false
+    let timer
+
+    let startTimer = function () {
+        if (!timerStarted) {
+            timer = new Promise(resolve => {
+                let initiateTimer = function () {
+                    console.log('started')
+                    setTimeout(() => resolve({ timeout: true }), 10000);
+                    timerStarted = true;
+                    editableContent.removeEventListener('keydown', initiateTimer)
+                }
+
+                editableContent.addEventListener('keydown', initiateTimer)
+            });
+        }
+    }
+
+    for (let i = 0; i < text.length; i++) {
+        blinkHandler(true, i)
+        const letter = text[i];
+
+        try {
+            const result = await Promise.race([handleInput(letter, startTimer()), timer]);
+            console.log(result)
+            if (result.timeout) {
+                console.log('Timeout')
+                break
+            }
+
+            const { input, preventFlag } = result
+
+            if (preventFlag) {
+                i--;
+                continue;
+            }
+
+            if (input === 'Escape') {
+                break;
+            }
+
+            if (input === 'Backspace') {
+                i = handleBackspace(i);
+                continue
+            }
+
+            if (input === ' ') {
+                blinkHandler(false, i)
+                continue
+            }
+
+            const className = validateInput(input, letter);
+            highlightText(i, className)
+
+            blinkHandler(false, i)
+        } catch (e) {
+            console.log(`Error: ${e}`)
+        }
+
+    }
+}
+
 function handleInput(letter) {
     let preventFlag = false;
 
@@ -26,40 +90,6 @@ function handleInput(letter) {
     });
 }
 
-async function main() {
-    wrapText()
-
-    for (let i = 0; i < text.length; i++) {
-        blinkHandler(true, i)
-        const letter = text[i];
-        const { input, preventFlag } = await handleInput(letter);
-
-        if (preventFlag) {
-            i--;
-            continue;
-        }
-
-        if (input === 'Escape') {
-            break;
-        }
-
-        if (input === 'Backspace') {
-            i = handleBackspace(i);
-            continue
-        }
-
-        if (input === ' ') {
-            blinkHandler(false, i)
-            continue
-        }
-
-        const className = validateInput(input, letter);
-        highlightText(i, className)
-
-        blinkHandler(false, i)
-    }
-}
-
 function getText() {
     text = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
     placeholder.innerText = text
@@ -67,20 +97,3 @@ function getText() {
 
 getText()
 main()
-
-
-
-
-
-// function moveCursorToEnd() {
-//     const range = document.createRange();
-//     const selection = window.getSelection();
-
-//     if (editableContent.lastChild) {
-//         range.setStartAfter(editableContent.lastChild);
-//         range.collapse(true);
-
-//         selection.removeAllRanges();
-//         selection.addRange(range);
-//     }
-// }
